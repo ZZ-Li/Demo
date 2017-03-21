b47493bdd8368b769a7b58cc96324ebd590d3d23
zz不知道 <zz821199495@163.com>
2017-03-21 17:57:07 +0800

    3-21

--- app/src/main/java/com/example/lzz/tablayouttest/MainActivity.java
+++ app/src/main/java/com/example/lzz/tablayouttest/MainActivity.java
@@ -1,18 +1,14 @@
 package com.example.lzz.tablayouttest;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,9 +23,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
-        navigationView = (NavigationView)findViewById(R.id.nav_view);
-        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 //        ActionBar actionBar = getSupportActionBar();
 //        if (actionBar != null){
@@ -38,9 +34,8 @@ public class MainActivity extends AppCompatActivity {
 //        }
 
 
-
         mainFragment = MainFragment.newInstance();
-        if (!mainFragment.isAdded()){
+        if (!mainFragment.isAdded()) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.layout_fragment, mainFragment, "MainFragment")
                     .commit();
@@ -64,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case android.R.id.home:
                 drawer.openDrawer(GravityCompat.START);
                 break;
